7c80b754e7804dafb8748cd53c73838edafe5a0b
Ryan Mah <rmah@uwaterloo.ca>
2024-02-06 21:02:39 -0500

    clean up a bit

--- Sources/NINearbyAccessorySample/GuidingLiteViewController.swift
+++ Sources/NINearbyAccessorySample/GuidingLiteViewController.swift
@@ -38,7 +38,8 @@ func decodeJSONString(_ jsonString: String) -> [String: Any]? {
     }
 }
 
-class GuidingLiteViewController: UIViewController {
+class GuidingLiteViewController: UIViewController
+{
     @IBOutlet weak var userArrowImage: UIImageView!
     @IBOutlet weak var guidingLiteSettingsButton: UIButton!
     
@@ -70,38 +71,46 @@ class GuidingLiteViewController: UIViewController {
     let mapBottomRight      = CGPoint(x: 359,   y: 736)
     
     let pinDefaultLocation  = CGPoint(x: 184.5, y: 555.5)
+
+    var dataChannel = DataCommunicationChannel()
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        
-//        updateDirectionArrow(angle: Float(90))
+
         locationPinImage.isHidden = true
-        
+
         // GuidingLite: heartbeat timer
         _ = Timer.scheduledTimer( timeInterval: 10,
                                   target: self,
                                   selector: #selector(self.send_mqtt_heartbeat),
                                   userInfo: nil,
                                   repeats: true )
-        
-        // Initialises the Timer used for Haptic and Sound feedbacks
+
+        // Main UI timer, 200ms
         _ = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(timerHandler), userInfo: nil, repeats: true)
-        
-        showIPAddressInputDialog()
-        
-        // Do any additional setup after loading the view.
+
+        // showIPAddressInputDialog()
+
+        dataChannel.accessoryDiscoveryHandler = self.anchor_discovered_handler
     }
     
+    func anchor_discovered_handler(index: Int)
+    {
+        print("Anchor discovered!")
+    }
+
     
-    func showIPAddressInputDialog() {
+    func showIPAddressInputDialog()
+    {
         self.first_time_mqtt_init = false
 
         let alertController = UIAlertController(title: "Enter IP Address", message: nil, preferredStyle: .alert)
-        
+
         alertController.addTextField { (textField) in
             textField.placeholder = "IP Address"
         }
-        
+
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         let okAction = UIAlertAction(title: "OK", style: .default) { [weak self] (_) in
             if let ipAddress = alertController.textFields?.first?.text {
@@ -115,74 +124,32 @@ class GuidingLiteViewController: UIViewController {
                 strongSelf.mqtt_client.connect()
             }
         }
-        
+
         alertController.addAction(cancelAction)
         alertController.addAction(okAction)
-        
+
         present(alertController, animated: true, completion: nil)
     }
     
-    @objc func timerHandler() {
+    @objc func timerHandler()
+    {
         // TODO: Implement the settings page similar to Qorvo demo and add settings view so I can uncomment all this.
         // ---------------------------------------------------------
         // MQTT SHIT
-//        if ( self.first_time_mqtt_init )
-//        {
-//            showIPAddressInputDialog()
-//        }
+        if ( self.first_time_mqtt_init )
+        {
+            self.showIPAddressInputDialog()
+        }
         // ---------------------------------------------------------
-        updateUserArrowPos(pos: mqtt_handler.userPosition)
-        updateDirectionArrow(angle: mqtt_handler.arrowAngle)
-        
-        // Feedback only enabled if the Qorvo device started ranging
-//        if (!appSettings.audioHapticEnabled! || feedbackDisabled ) {
-//            return
-//        }
-        
-//        if selectedAccessory == -1 {
-//            return
-//        }
-        
-//        let qorvoDevice = dataChannel.getDeviceFromUniqueID(selectedAccessory)
-        
-//        if qorvoDevice?.blePeripheralStatus != statusRanging {
-//            return
-//        }
-        
-        // As the timer is fast timerIndex and timerIndexRef provides a
-        // pre-scaler to achieve different patterns
-//        if  timerIndex != feedbackPar[feedbackLevel].timerIndexRef {
-//            timerIndex += 1
-//            return
-//        }
-       
-//        timerIndex = 0
 
-        // Handles Sound, if enabled
-//        let systemSoundID: SystemSoundID = 1052
-//        AudioServicesPlaySystemSound(systemSoundID)
-
-        // Handles Haptic, if enabled
-//        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-//        var events = [CHHapticEvent]()
-
-//        let humm = CHHapticEvent(eventType: .hapticContinuous,
-//                                 parameters: [],
-//                                 relativeTime: 0,
-//                                 duration: feedbackPar[feedbackLevel].hummDuration)
-//        events.append(humm)
-
-//        do {
-//            let pattern = try CHHapticPattern(events: events, parameters: [])
-//            let player = try engine?.makePlayer(with: pattern)
-//            try player?.start(atTime: 0)
-//        } catch {
-//            logger.info("Failed to play pattern: \(error.localizedDescription).")
-//        }
+        self.updateUserArrowPos(pos: mqtt_handler.userPosition)
+        self.updateDirectionArrow(angle: mqtt_handler.arrowAngle)
     }
     
-    @IBAction func goButtonPressed(_ sender: UIButton) {
+    @IBAction func goButtonPressed(_ sender: UIButton)
+    {
         print("Go Pressed!")
+
         // Add your logic here
         if(currDestState == S_SET_DEST) {
             self.send_mqtt_pin_location()
@@ -193,7 +160,7 @@ class GuidingLiteViewController: UIViewController {
     @IBAction func setButtonPressed(_ sender: UIButton) {
         print("set  Pressed!")
         
-        updateLocationPinImage(pos: pinDefaultLocation)
+        self.updateLocationPinImage(pos: pinDefaultLocation)
         locationPinImage.isHidden = false
         currDestState = S_SET_DEST
     }
@@ -223,34 +190,41 @@ class GuidingLiteViewController: UIViewController {
         }
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
         // Get the first touch (assuming single touch)
         if let touch = touches.first {
             // Update position when the touch begins
-            updatePinPosition(for: touch)
+            self.updatePinPosition(for: touch)
         }
     }
 
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
         // Get the first touch (assuming single touch)
         if let touch = touches.first {
             // Update position while the touch is moving
-            updatePinPosition(for: touch)
+            self.updatePinPosition(for: touch)
         }
     }
 
-    func updatePinPosition(for touch: UITouch) {
-        if(currDestState == S_SET_DEST) {
+    func updatePinPosition(for touch: UITouch)
+    {
+        if (currDestState == S_SET_DEST)
+        {
             // Only update pinLocation when setting a new destination
             pinLocation = touch.location(in: self.view)
             print("Live update - Touch coordinates: x = \(pinLocation.x), y = \(pinLocation.y)")
-            if (isPointInMap(pos: pinLocation)) {
-                updateLocationPinImage(pos: pinLocation)
+
+            if ( self.isPointInMap(pos: pinLocation) )
+            {
+                self.updateLocationPinImage(pos: pinLocation)
             }
         }
     }
     
-    func updateDirectionArrow(angle: Float) {
+    func updateDirectionArrow(angle: Float)
+    {
         UIView.animate(withDuration: 0.5) {
             // Convert the angle to radians
             let radians = angle * .pi / 180.0
@@ -259,30 +233,42 @@ class GuidingLiteViewController: UIViewController {
             self.directionArrowImage.transform = CGAffineTransform(rotationAngle: CGFloat(radians))
         }
     }
-    func updateUserArrowPos(pos: CGPoint) {
+
+    func updateUserArrowPos(pos: CGPoint)
+    {
         var scaled_coord = CGPoint(x: (mapBottomLeft.x + pos.x), y: (mapBottomLeft.y - pos.y))
         
         // Before updating the position coordinate, make sure that this point does not exceed
         // the map bounds.
-        if(scaled_coord.x >= mapTopRight.x) {
+        if (scaled_coord.x >= mapTopRight.x)
+        {
             scaled_coord.x = mapTopRight.x
-        } else if(scaled_coord.x <= mapTopLeft.x) {
+        }
+        else if (scaled_coord.x <= mapTopLeft.x)
+        {
             scaled_coord.x = mapTopLeft.x
         }
         
-        if(scaled_coord.y >= mapBottomRight.y) {
+        if (scaled_coord.y >= mapBottomRight.y)
+        {
             scaled_coord.y = mapBottomRight.y
-        } else if(scaled_coord.y <= mapTopRight.y) {
+        }
+        else if(scaled_coord.y <= mapTopRight.y)
+        {
             scaled_coord.y = mapTopRight.y
         }
         
         
         userArrowImage.frame.origin = scaled_coord
     }
-    func updateLocationPinImage(pos: CGPoint) {
+
+    func updateLocationPinImage(pos: CGPoint)
+    {
         locationPinImage.frame.origin = pos
     }
-    func isPointInMap(pos: CGPoint) -> Bool {
+
+    func isPointInMap(pos: CGPoint) -> Bool
+    {
         let x = pos.x
         let y = pos.y
         
@@ -293,10 +279,10 @@ class GuidingLiteViewController: UIViewController {
     }
     
     // Returns the current position of the user arrow relative to the map area.
-    func getUserArrowPos() -> CGPoint {
+    func getUserArrowPos() -> CGPoint
+    {
         return CGPoint(x: 0, y: 0)
     }
-    
     
     /*
     // MARK: - Navigation
@@ -307,7 +293,4 @@ class GuidingLiteViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-    
-    
-    
 }
