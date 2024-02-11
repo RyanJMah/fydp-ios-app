300673d5124fb5a14aa8aa06d931cb97df300f1e
Ryan Mah <rmah@uwaterloo.ca>
2024-02-10 23:41:01 -0500

    add prompt

--- Sources/NINearbyAccessorySample/GuidingLite_IMU.swift
+++ Sources/NINearbyAccessorySample/GuidingLite_IMU.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2023 Apple. All rights reserved.
 //
 
+import UIKit
 import Foundation
 import CoreLocation
 
@@ -18,6 +19,9 @@ class GuidingLite_HeadingSensor: NSObject, CLLocationManagerDelegate {
         super.init()
         
         locationManager.delegate = self
+
+        locationManager.headingOrientation = CLDeviceOrientation.faceUp
+
         locationManager.startUpdatingHeading()
         
         if ( CLLocationManager.headingAvailable() )
@@ -41,10 +45,21 @@ class GuidingLite_HeadingSensor: NSObject, CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading)
     {
         // We can handle orientation update events here (if we need to in the future)
+        if newHeading.headingAccuracy < 0 || newHeading.headingAccuracy > 30 {
+            // Heading accuracy is poor, suggest recalibration
+            displayRecalibrationAlert()
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error)
     {
         print("Location manager error: \(error.localizedDescription)")
+    }
+
+    func displayRecalibrationAlert() {
+        let alertController = UIAlertController(title: "Compass Accuracy", message: "Compass accuracy is poor. Consider recalibrating.", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        // Present the alert on the topmost view controller
+        UIApplication.shared.keyWindow?.rootViewController?.present(alertController, animated: true, completion: nil)
     }
 }
